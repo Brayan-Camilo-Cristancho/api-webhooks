74e4185f69e92e0d0880f53c277127657c06cfee
Brayan Cristancho <brayan.cristanchof@gmail.com>
2025-08-18 15:06:50 -0500

    init

--- src/routes/routes.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Application } from 'express';
-export declare function setRoutes(app: Application): void;
-//# sourceMappingURL=routes.d.ts.map
--- src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-export function setRoutes(app) {
-    app.get('/', (req, res) => {
-        res.json({ message: 'API funcionando con TypeScript' });
-    });
-    app.get('/test-worker', (req, res) => {
-        res.json({ message: 'API funcionando con TypeScript' });
-    });
-}
-//# sourceMappingURL=routes.js.map
