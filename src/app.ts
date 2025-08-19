e2939dbb72f8bda8e6554d0200d5c6fe1249a6cb
Brayan Cristancho <brayan.cristanchof@gmail.com>
2025-08-19 17:33:28 -0500

    init

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,6 @@ app.use(someMiddleware);
 
 setRoutes(app);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
